c570cf1d8d81acd59b3478d96336f4f1374ef933
Lavergnat Jérémie <lavergnatj@outlook.fr>
2024-02-27 10:41:53 +0100

    Added an async function to retrieve works and another to display them on the DOM

---
+++ script.js
@@ -0,0 +1,26 @@
+//Get all the works stored in the API and transform the data in JSON
+async function getWorks() {
+  const fetchWorks = await fetch("http://localhost:5678/api/works");
+  worksJson = await fetchWorks.json();
+  return worksJson;
+}
+
+//Update works in the DOM by creating a figure who contains and img and a figcaption
+async function updateWorks() {
+  const works = await getWorks();
+  const gallery = document.querySelector(".gallery");
+
+  works.forEach((work) => {
+    const figure = document.createElement("figure");
+    gallery.appendChild(figure);
+    const imgFigure = document.createElement("img");
+    imgFigure.src = work.imageUrl;
+    imgFigure.alt = work.title;
+    figure.appendChild(imgFigure);
+    const imgFigcaption = document.createElement("figcaption");
+    imgFigcaption.textContent = work.title;
+    figure.appendChild(imgFigcaption);
+  });
+}
+
+updateWorks();
