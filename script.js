6739b289b9b24201a59ffd1d657d6022ec786d0b
Lavergnat Jérémie <lavergnatj@outlook.fr>
2024-02-27 10:52:55 +0100

    Add error management if API doesn't respond

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //Get all the works stored in the API and transform the data in JSON
 async function getWorks() {
-  const fetchWorks = await fetch("http://localhost:5678/api/works");
-  worksJson = await fetchWorks.json();
-  return worksJson;
+  try {
+    const fetchWorks = await fetch("http://localhost:5678/api/works");
+    worksJson = await fetchWorks.json();
+    return worksJson;
+  } catch (error) {
+    console.error("Failed to fetch data from API", error);
+  }
 }
 
 //Update works in the DOM by creating a figure who contains and img and a figcaption
